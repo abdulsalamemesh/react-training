b293c72e5580a36b78a7f38900260b2ae359dea5
Abdulsalam Emesh <Abdulsalam@meier.local>
2019-04-23 16:08:38 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  state = {
+    persons: [
+      { name: "Abod", age: 27 },
+      { name: "Mayar", age: 23 },
+      { name: "Ali", age: 25 }
+    ]
+  };
+
+  switchNameHandler = newName => {
+    this.setState({
+      persons: [
+        { name: newName, age: 27 },
+        { name: "Mayar", age: 23 },
+        { name: "Ali", age: 26 }
+      ]
+    });
+  };
+
+  nameChangedHandler = event => {
+    this.setState({
+      persons: [
+        { name: "Abdulsalam", age: 27 },
+        { name: event.target.value, age: 23 },
+        { name: "Ali", age: 26 }
+      ]
+    });
+  };
+  render() {
+    return (
+      <div className="App">
+        <h1> Hi I'm a React App</h1>
+        <button onClick={this.switchNameHandler.bind(this, "Abdulsalam")}>
+          Switch Names
+        </button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, "the King")}
+          changed={this.nameChangedHandler}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          my Hobby: swimmng
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
